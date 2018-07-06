2b7c1bf20ca85be7144d6ebb8bb13f87c96fe639
Mahendra <mahendrasingh9211@gmail.com>
2018-07-06 22:50:51 +0530

    pickup
    addpickup

---
+++ QDCCustomerApp/PickUp/Controllers/PickUpVC.swift
@@ -0,0 +1,25 @@
+//
+//  PickUpVC.swift
+//  QDCCustomerApp
+//
+//  Created by Maahi on 06/07/18.
+//  Copyright © 2018 QuickDryCleaning. All rights reserved.
+//
+
+import UIKit
+
+class PickUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
+extension PickUpVC{
+    
+}
