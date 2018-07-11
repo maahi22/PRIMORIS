4f67a93d0c47cc91f987549ff0d0865aa4ba9f54
Mahendra <mahendrasingh9211@gmail.com>
2018-07-11 16:12:57 +0530

    Profile changes
    Profile changes

---
+++ QDCCustomerApp/Profile/ViewModel/ProfileClient.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileClient.swift
+//  QDCCustomerApp
+//
+//  Created by Maahi on 11/07/18.
+//  Copyright © 2018 QuickDryCleaning. All rights reserved.
+//
+
+import UIKit
+
+class ProfileClient: NSObject {
+
+    // MARK: - Injections
+    internal let networkClient = NetworkClient.shared
+    
+    
+    
+    func updateProfile( areaLocation:String,
+                            name:String,
+                            address:String,
+                            
+                            completion:@escaping (CustomerCreationModel?,String)->())  {
+        
+        let dbName = QDCUserDefaults.getDataBaseName()
+        let clientID = QDCUserDefaults.getClientID()
+        let branchID = QDCUserDefaults.getBranchId()
+        let customerCode = QDCUserDefaults.getCustomerId()
+        let token = QDCUserDefaults.getAccessToken()
+        
+        var params = ["AreaLocation": areaLocation,
+                      "Name":name,
+                      "Address": address,
+                      "CustomerCode" :customerCode,
+                      "DatabaseName" : clientID,
+                      "BranchID":branchID ] as [String:Any]
+        
+        let headers = ["token": "\(token)", "Content-Type": "application/json"] as [String:String]
+        
+        let apiname = UPDATE_CUSTOMER_PROFILE_RELATIVE_URL
+        
+        networkClient.callAPIWithAlamofire(apiname: apiname,
+                                           requestType: .post,
+                                           params: params,
+                                           headers: headers,
+                                           success: { (data, httpResponse) in
+                                            
+                                            if let customerCreationModel = decodeJSON(type: CustomerCreationModel.self, from: data) {
+                                                completion(customerCreationModel, "Success")
+                                            }else{
+                                                completion(nil,"failed")
+                                            }
+                                            
+        }) { (error, message) in
+            
+            completion(nil,message)
+            
+        }
+    }
+    
+    
+}
